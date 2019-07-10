1be0d4a6799cbddd6784acabfc5a5dc46ff6756f
Sourabh Zambre <sourabhzambre123@gmail.com>
2019-07-10 21:34:12 +0530

    Update DataStructures.java

--- DataStructures.java
+++ DataStructures.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package datastructures;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -25,23 +18,37 @@ public class DataStructures {
        
         FileOutputStream fout = new FileOutputStream("C:\\Users\\Hp\\Desktop\\MyLog.txt"); 
         PrintStream out = new PrintStream(fout) ;
+        
         if ( !f.exists()) {
             System.err.println("file path not specified");
         }
         try {
             String regex1 = "ENERGY:\\s(.*)";//"[0-9]+\|INFO\|MyHalfDuplexModem@[0-9]+:println\|ENERGY:";
             final Pattern p = Pattern.compile(regex1);
-            
+            String regex2 = "[0-9]+";//"[0-9]+\|INFO\|MyHalfDuplexModem@[0-9]+:println\|ENERGY:";
+            final Pattern q = Pattern.compile(regex2);
+            String regex3 = "-eT";//"[0-9]+\|INFO\|MyHalfDuplexModem@[0-9]+:println\|ENERGY:";
+            final Pattern r = Pattern.compile(regex3);
             Scanner sc = new Scanner(f);
-     
-
+            long x = 0,y=0;
+            int count = 0;
+             
                 while (sc.hasNextLine()) {
                    
                     String nextLine = sc.nextLine();
+                    Matcher n = q.matcher(nextLine);
                     Matcher m = p.matcher(nextLine);
-                    if (m.find()) {
-                       System.out.println(m.group(0)); // whole matched expression
-                       out.append(m.group(0)+"\n") ;
+                    Matcher o = r.matcher(nextLine);
+                    
+                    if (m.find() && n.find() && o.find()) {
+                       // System.out.println(n.group(0));
+                       y = Long.parseLong(n.group(0));
+                       if(count==0){
+                           x = Long.parseLong(n.group(0));
+                        }
+                       System.out.println((y-x)+" "+m.group(0)); // whole matched expression
+                       out.append((y-x)+" "+m.group(0)+"\n") ;
+                       count++;
                        }
                             
                 }  
@@ -52,5 +59,3 @@ public class DataStructures {
              }
     }
 }
-    
-
