6b23952ade739c1a42fa96b72ba180d6e3713a16
Sourabh Zambre <sourabhzambre123@gmail.com>
2020-07-24 20:37:00 +0530

    Update MusicPlay.java

--- Java project Process_Synchronization/MusicPlay.java
+++ Java project Process_Synchronization/MusicPlay.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -71,9 +66,7 @@ class Thread_songs extends Thread{
 
 public class MusicPlay {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args)throws InterruptedException  {
         // TODO code application logic here
           Speaker speaker = new Speaker();
